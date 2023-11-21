e397af9ba19ea2eec2c6f650e71667d0e6f7f52b
Domitrii <126395589+Domitrii@users.noreply.github.com>
2023-11-21 16:31:31 +0100

    Update task-4.js

--- js/task-4.js
+++ js/task-4.js
@@ -6,7 +6,7 @@ const getTotalBalanceByGender = (users, gender) => {
 
 
 
-const allUsers = [
+const all = [
     {
     name: "Moore Hensley",
     gender: "male",
@@ -44,6 +44,6 @@ const allUsers = [
   }
 ];
 
-console.log(getTotalBalanceByGender(allUsers, "male")); // 12053
+console.log(getTotalBalanceByGender(all, "male")); // 12053
 
-console.log(getTotalBalanceByGender(allUsers, "female")); // 8863
+console.log(getTotalBalanceByGender(all, "female")); // 8863
